58641b640cb84772c5e7a79e124b81ffe1973297
is-lgarcia <luis10.g@hotmail.com>
2020-05-20 16:31:47 -0600

    Defincion de clases POJO para Login y SingUp

---
+++ app/src/main/java/com/luisg/minitwitter/RequestLogin.java
@@ -0,0 +1,40 @@
+
+package com.luisg.minitwitter;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class RequestLogin {
+
+    @SerializedName("email")
+    @Expose
+    private String email;
+    @SerializedName("password")
+    @Expose
+    private String password;
+
+    public RequestLogin(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public RequestLogin() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
---
+++ app/src/main/java/com/luisg/minitwitter/RequestSignUp.java
@@ -0,0 +1,64 @@
+
+package com.luisg.minitwitter;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class RequestSignUp {
+
+    @SerializedName("username")
+    @Expose
+    private String username;
+    @SerializedName("email")
+    @Expose
+    private String email;
+    @SerializedName("password")
+    @Expose
+    private String password;
+    @SerializedName("code")
+    @Expose
+    private String code;
+
+    public RequestSignUp() {
+    }
+
+    public RequestSignUp(String username, String email, String password, String code) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.code = code;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+}
