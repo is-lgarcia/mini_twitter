cba41a16853ad105e3b829019d081f1422820070
is-lgarcia <luis10.g@hotmail.com>
2020-05-22 01:01:11 -0600

    Implementando Clases POJO para Tweet, Like y User

--- app/src/main/java/com/luisg/minitwitter/view/ui/home/TwettViewModel.java
+++ app/src/main/java/com/luisg/minitwitter/view/ui/home/TwettViewModel.java
@@ -4,11 +4,11 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class HomeViewModel extends ViewModel {
+public class TwettViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public HomeViewModel() {
+    public TwettViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is home fragment");
     }
